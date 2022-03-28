0655a7e5cf49abd24e6f6f98e1273f68768381d4
nmdemirkol <34094391+nmdemirkol@users.noreply.github.com>
2022-03-28 18:41:06 +0300

    Bakkal Asp.net MVC
    Bakkal Asp.net MVC

---
+++ bakkal181213086/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bakkal181213086.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
